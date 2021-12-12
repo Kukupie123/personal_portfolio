54673195cba661a61a537d222910e96733c0fb82
kukukodes <45627511+Kukupie123@users.noreply.github.com>
2021-12-12 22:55:16 +0530

    added clouds night theme

--- src/pages/home/home_main.js
+++ src/pages/home/home_main.js
@@ -9,6 +9,9 @@ import '../../styles/home.css'
 import '../../styles/neon.css'
 import '../../styles/glow.css'
 
+//Clouds
+import Cloud from '../../assets/cloud.png'
+
 
 let Home = () => {
     //Scrolling Data section
@@ -21,18 +24,14 @@ let Home = () => {
     useEffect(() => {
         //Equivalent to componentDidMount
         window.addEventListener('scroll', handleScroll)
-        if (eyeHor != undefined)
-            eyeHor.value = 0;
-        if (eyeVer != undefined)
-            eyeVer.value = 0;
+        if (eyeHor != undefined) eyeHor.value = 0;
+        if (eyeVer != undefined) eyeVer.value = 0;
 
         //Equivalent to componentDidUnmount
         return () => {
             window.removeEventListener('scroll', handleScroll);
         }
-    }, [])
-
-
+    }, 6)
     //DEBUG
     // eslint-disable-next-line no-unused-vars
     const [xp, setXP] = useState(0)
@@ -54,7 +53,7 @@ let Home = () => {
 
     const onMouseMove = (e) => {
         if (e.currentTarget == undefined) return
-        if (eyeVer.value == undefined || eyeHor.value == undefined) return
+        if (eyeVer == undefined || eyeHor == undefined) return
 
         //Get size of screen
         const heightY = ref.current.clientHeight;
@@ -89,80 +88,130 @@ let Home = () => {
         setXP(perX_Width)
         setYP(perY_Height)
 
+    }
+
+    //Framer motion Variants
+
+    const getCloudVariant = (duration, shouldGoLeft) => {
+
+        let variant = {
+            visible: {
+                transition: {
+                    yoyo: Infinity,
+                    duration: duration,
+                    ease: "linear"
+
+                }
+            }
+        }
+
+        if (shouldGoLeft == false) {
+            variant.visible.right = 0;
+            return variant;
+        }
+        variant.visible.left = 0;
+        return variant;
+
 
     }
+
+
+    const getRandomHeightForCloud = (max, min) => {
+        const height = Math.random() * (max - min) + min;
+
+        return {
+            top: height + "px"
+        }
+    }
+
 
     return (
 
         <div className="home" ref={ref} onMouseMove={onMouseMove}>
+            {/*Moving Cloud section*/}
+
+            <div className="cloud-container filter-dark">
+
+                <motion.img src={Cloud} className="cloud" style={{
+                    top: "20px"
+                }}
+                            variants={getCloudVariant(55, false)}
+                            animate="visible"
+                />
+                <motion.img src={Cloud} className="cloud right" style={{
+                    top: "40px"
+                }}
+                            variants={getCloudVariant(90, true)}
+                            animate="visible"
+                />
+            </div>
+
+            <div className="cloud-container filter-dark left">
+
+                <motion.img src={Cloud} className="cloud" style={{
+
+                    top: "60px"
+                }}
+
+                            variants={getCloudVariant(40, false)}
+                            animate="visible"
+                />
+                <motion.img src={Cloud} className="cloud right" style={{
+                    top: "80px"
+                }}
+                            variants={getCloudVariant(60, true)}
+                            animate="visible"
+                />
+            </div>
 
 
-            <section className="avatar_buttons">
+            <div className="avatar-button-container">
+                {/*My Avatar Section and button*/}
                 <RiveComponent className='avatar'/>
+
+                {/*ButtonContainer to align buttons*/}
                 <div className="btn-container">
                     <motion.button className='btn-2 neonTextWhite flicker  '
-                                   initial={
-                                       {
-                                           scale: 0,
-                                           left: "-100vw",
-                                           top: "-100vh"
+                                   initial={{
+                                       scale: 0, left: "-100vw", top: "-100vh",
 
 
-                                       }
-                                   }
+                                   }}
 
                                    animate={{
-                                       left: 0,
-                                       top: 0,
-                                       scale: 1
-                                   }}
-
-                                   transition={{
-                                       delay: 0.51,
+                                       left: 0, top: 0, scale: 1,
 
                                    }}
-                                   whileHover={
-                                       {
-                                           backgroundColor: "rgba(100,0,0,0.5)",
-                                           color: "pink",
-                                           borderTopLeftRadius: "5px",
-                                       }
-                                   }
-                    >Technical Side
+
+
+                                   whileHover={{
+                                       backgroundColor: "rgba(100,0,0,0.5)", color: "pink", borderTopLeftRadius: "5px",
+                                   }}
+                    >Nerd's corner
                     </motion.button>
+
                     <motion.button className='btn-1 neonText subtleFlick '
-                                   initial={
-                                       {
-                                           scale: 0,
-                                           right: "-100vw",
-                                           bottom: "-100vh"
+                                   initial={{
+                                       scale: 0, right: "-100vw", bottom: "-100vh"
 
 
-                                       }
-                                   }
+                                   }}
 
                                    animate={{
-                                       right: 0,
-                                       bottom: 0,
-                                       scale: 1
+                                       right: 0, bottom: 0, scale: 1
                                    }}
 
-                                   transition={{
-                                       delay: 0.51,
+
+                                   whileHover={{
+                                       backgroundColor: "rgba(0,0,0,0.5)", color: "pink", borderTopRightRadius: "5px",
 
                                    }}
-                                   whileHover={
-                                       {
-                                           backgroundColor: "rgba(0,0,0,0.5)",
-                                           color: "pink",
-                                           borderTopRightRadius: "5px",
-
-                                       }
-                                   }
-                    >Artistic Side
+                    >Artistic World
                     </motion.button>
                 </div>
-            </section>
+
+            </div>
+
         </div>
 
 
