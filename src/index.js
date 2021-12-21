33303a0a832d4eb2e65e34eca5b11932cc6fd7ab
kukukodes <45627511+Kukupie123@users.noreply.github.com>
2021-12-21 15:19:03 +0530

    nerd corner page started

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Home from "./pages/home/home_main";
-
+import NerdCorner from "./pages/nerd corner/Nerd_Corner";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 ReactDOM.render
 (
-    <Home/>,
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/nerd_corner" element={<NerdCorner/>}/>
+        </Routes>
+    </BrowserRouter>,
     document.getElementById('root')
 )
---
+++ src/pages/nerd corner/Nerd_Corner.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles/nerdcorner.css"
+
+const NerdCorner = () => {
+    return (
+        <div className="bg">
+
+            {/*Glassmorphism container*/}
+            <div className="glass second-container">
+                Second container bro
+            </div>
+
+
+        </div>
+    )
+}
+export default NerdCorner;
